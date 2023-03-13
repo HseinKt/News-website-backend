936713fcb822dee0140d5c80436aa85ba2ca3322
HseinKt <hseinkteish1998@gmail.com>
2023-03-13 21:07:44 +0200

    added connection dataBase

---
+++ connection_db.php
@@ -0,0 +1,16 @@
+<?php
+$host_name = 'localhost';
+$database = 'newswebsite_db';
+$username = 'root';
+$password = '';
+
+$mysqli = new mysqli($host_name, $username, $password, $database);
+
+header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Headers:*");
+if ($mysqli->connect_error) {
+    die('<p>Failed to connect to MySQL: ' . $mysqli->connect_error . '</p>');
+}
+
+
+?>

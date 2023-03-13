a4d846780447b0189974fa4df07e91f67edc58ee
HseinKt <hseinkteish1998@gmail.com>
2023-03-13 23:55:25 +0200

    updated

--- index_api.php
+++ index_api.php
@@ -1,7 +1,9 @@
 <?php
 include "connection_db.php";
 
-$sql_query = $mysqli -> query('SELECT * FROM articles');
+$sql_query = $mysqli -> query('SELECT ar.*, a.name as author_name 
+FROM articles ar
+JOIN authors a ON a.id = ar.author_id ');
 
 $response = [];
 
@@ -10,8 +12,9 @@ while ($row = $sql_query -> fetch_array(MYSQLI_NUM)) {
     $arr['id'] = $row[0];
     $arr['title'] = $row[1];
     $arr['body'] = $row[2];
-    $arr['author_id '] = $row[3];
-    $arr['category_id '] = $row[4];
+    $arr['author_id'] = $row[3];
+    $arr['category_id'] = $row[4];
+    $arr['author_name'] = $row[5];
 
     array_push($response, $arr);
 }
